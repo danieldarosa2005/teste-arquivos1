4f8ecf71ba4b5ce20c345b54ab984270d3a94d1c
Daniel da Rosa <danieldarosa170@gmail.com>
2025-05-02 17:32:15 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+System.Console.WriteLine("Digite o caminho completo do arquivo:");
+string arquivoOrigem = Console.ReadLine();
+
+try {
+    string [] linhas = File.ReadAllLines(arquivoOrigem);
+
+    string pastaFonte = Path.GetDirectoryName(arquivoOrigem);
+    string pastaDestino = pastaFonte + @"\saida";
+    string arquivoDestino = pastaDestino + @"\resumo.csv";
+    
+    Directory.CreateDirectory(pastaDestino); //pasta saída
+
+    using (StreamWriter sw = File.AppendText(arquivoDestino)) {
+        foreach (string item in linhas)
+        {
+            string [] campo = item.Split(',');
+            string nome = campo[0];
+            double preco = double.Parse(campo[1], CultureInfo.InvariantCulture);
+            int quantidade = int.Parse(campo[2]);
+
+            Produto produto = new Produto(nome,preco,quantidade);
+            sw.WriteLine(produto.Nome + "," + produto.Soma().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
+catch (IOException e) {
+    System.Console.WriteLine("Ocorreu um erro");
+    System.Console.WriteLine(e.Message);
+}
